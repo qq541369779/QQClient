44508d7764d447bd77b8afe1151862c9d67a7c25
ace <541369779@qq.com.com>
2022-10-28 15:32:49 +0800

    客户端新增文件发送功能

--- src/com/hspedu/qqclient/service/ClientConnectServerThread.java
+++ src/com/hspedu/qqclient/service/ClientConnectServerThread.java
@@ -3,6 +3,7 @@ package com.hspedu.qqclient.service;
 import com.hspedu.qqcommon.Message;
 import com.hspedu.qqcommon.MessageType;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.Socket;
@@ -50,6 +51,15 @@ public class ClientConnectServerThread extends Thread {
                 } else if (message.getMesType().equals(MessageType.MESSAGE_TO_ALL_MES)) {
                     // 显示在客户端的控制台
                     System.out.println("\n" + message.getSender() + "对大家说" + message.getContent());
+
+                } else if (message.getMesType().equals(MessageType.MESSAGE_FILE_MES)) {// 如果是文件消息
+                    System.out.println("\n" + message.getSender() + "给" + message.getGetter() + "发文件：" + message.getSrc()
+                            + "到我的电脑的目录：" + message.getDest());
+                    // 取出message的文件字节数组，通过文件输出流写出到磁盘
+                    FileOutputStream fileOutputStream = new FileOutputStream(message.getDest());
+                    fileOutputStream.write(message.getFileBytes());
+                    fileOutputStream.close();
+                    System.out.println("保存文件成功");
 
                 } else {
                     System.out.println("其他类型的message，暂时不处理");
--- src/com/hspedu/qqclient/view/QQView.java
+++ src/com/hspedu/qqclient/view/QQView.java
@@ -1,6 +1,7 @@
 package com.hspedu.qqclient.view;
 
 import com.hspedu.qqclient.Utility.Utility;
+import com.hspedu.qqclient.service.FileClientService;
 import com.hspedu.qqclient.service.MessageClientService;
 import com.hspedu.qqclient.service.UserClientService;
 
@@ -16,7 +17,8 @@ public class QQView {
     private boolean loop = true; //控制是否显示菜单
     private String key = ""; // 接收用户的键盘输入
     private UserClientService userClientService = new UserClientService();//对象用于登录服务/注册用户
-    private MessageClientService messageClientService = new MessageClientService();
+    private MessageClientService messageClientService = new MessageClientService();//该对象用于用户私聊/群聊
+    private FileClientService fileClientService = new FileClientService();//该对象用户传输文件
 
     public static void main(String[] args) {
         new QQView().mainMenu();
@@ -53,7 +55,7 @@ public class QQView {
                             System.out.println("\t\t 3 私聊消息");
                             System.out.println("\t\t 4 发送文件");
                             System.out.println("\t\t 9 退出系统");
-                            System.out.print("请输入你的选择：");
+                            System.out.println("请输入你的选择：");
                             key = Utility.readString(1);
 
                             switch (key) {
@@ -65,7 +67,7 @@ public class QQView {
                                     System.out.println("请输入想对大家说的话：");
                                     String s = Utility.readString(100);
                                     // 调用一个方法，将消息封装成message对象，发送给服务器
-                                    messageClientService.sendMessageToAll(s,userId);
+                                    messageClientService.sendMessageToAll(s, userId);
                                     break;
                                 case "3":
                                     System.out.println("请输入想聊天的用户号(在线)：");
@@ -77,7 +79,13 @@ public class QQView {
                                     System.out.println("私聊消息");
                                     break;
                                 case "4":
-                                    System.out.println("发送文件");
+                                    System.out.print("请输入你想把文件发送给的用户(在线用户)：");
+                                    getterId = Utility.readString(50);
+                                    System.out.print("请输入发送文件的路径(形式：d:\\xxx.jpg)：");
+                                    String src = Utility.readString(100);
+                                    System.out.print("请输入把文件发送到对方的路径(形式：d:\\xxx.jpg)：");
+                                    String dest = Utility.readString(100);
+                                    fileClientService.sendFileToOne(src, dest, userId, getterId);
                                     break;
                                 case "9":
                                     // 调用方法，给服务器发送一个退出系统的message
